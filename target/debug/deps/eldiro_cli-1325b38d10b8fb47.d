27227ba9fe6a8ae625b3e48f676f6cd0e8be2184
Horc-zh <127604084+Horc-zh@users.noreply.github.com>
2024-08-10 11:33:56 +0800

    Delete target directory

--- target/debug/deps/eldiro_cli-1325b38d10b8fb47.d
+++
@@ -1,5 +0,0 @@
-C:\Code\rust\eldiro\target\debug\deps\libeldiro_cli-1325b38d10b8fb47.rmeta: crates\eldiro-cli\src\main.rs
-
-C:\Code\rust\eldiro\target\debug\deps\eldiro_cli-1325b38d10b8fb47.d: crates\eldiro-cli\src\main.rs
-
-crates\eldiro-cli\src\main.rs:
